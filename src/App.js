860f01a531e96704c1d3d2706456a72c0ede6162
Amanullah Aman <amancse.aiub@gmail.com>
2023-07-16 15:33:59 +0600

    react memo callback code optimization techniques

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useCallback, useMemo } from "react";
+
+import "./App.css";
+import DemoList from "./components/Demo/DemoList";
+import Button from "./components/UI/Button/Button";
+
+function App() {
+  console.log("app is running");
+  const [listTitle, setListTitle] = useState("My List");
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+  return (
+    <div className="app">
+      <DemoList title={listTitle} items={useMemo(() => [5, 3, 1, 10, 9], [])} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+    </div>
+  );
+}
+
+export default App;
